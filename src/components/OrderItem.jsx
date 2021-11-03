edbdf2dfaf88a00411e20320205d9a742969cf71
Oscar Barajas <oscar@gndx.dev>
2021-11-03 17:32:03 -0500

    :zap: [ADD] Checkout

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -13,10 +13,10 @@ const OrderItem = ({ product }) => {
 	return (
 		<div className={styles.OrderItem}>
 			<figure>
-				<img src={product.images[0]} alt={product.title} />
+				<img src={product?.images[0]} alt={product?.title} />
 			</figure>
-			<p>{product.title}</p>
-			<p>${product.price}</p>
+			<p>{product?.title}</p>
+			<p>${product?.price}</p>
 			<img className={styles.pointer, styles['more-clickable-area']} src={close} alt="close" onClick={() => handleRemove(product)} />
 		</div>
 	);
