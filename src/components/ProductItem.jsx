1acfece74417aae88e12b33daf877e86d4af630b
Oscar Barajas <oscar@gndx.dev>
2021-11-03 17:12:43 -0500

    :zap: [ADD] Home

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
-import '@styles/ProductItem.scss';
-import AppContext from '../context/AppContext';
+import AppContext from '@context/AppContext';
 import addToCartImage from '@icons/bt_add_to_cart.svg';
 import addedToCartImage from '@icons/bt_added_to_cart.svg';
+import styles from '@styles/ProductItem.module.scss';
 
 const ProductItem = ({ product }) => {
 	const { state, addToCart } = useContext(AppContext);
@@ -13,16 +13,19 @@ const ProductItem = ({ product }) => {
 	}
 
 	return (
-		<div className="ProductItem">
+		<div className={styles.ProductItem}>
 			<img src={product.images[0]} alt={product.title} />
-			<div className="product-info">
+			<div className={styles['product-info']}>
 				<div>
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure className="more-clickable-area" onClick={() => handleClick(product)} >
-					{  state.cart.includes(product) ? <img className="disabled add-to-cart-btn" src={addedToCartImage} alt="added to cart" />  : <img className="add-to-cart-btn pointer" src={addToCartImage} alt="add to cart" /> }
-					
+				<figure className={styles['more-clickable-area']} onClick={() => handleClick(product)} >
+					{state.cart.includes(product) ? <img
+						className={styles.disabled, styles['add-to-cart-btn']}
+						src={addedToCartImage}
+						alt="added to cart"
+					/> : <img className={styles['add-to-cart-btn'], styles.pointer} src={addToCartImage} alt="add to cart" />}
 				</figure>
 			</div>
 		</div>
