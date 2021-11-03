abf1f021c5838943faabf0c32a6c699312ea80a4
Oscar Barajas <oscar@gndx.dev>
2021-11-03 16:53:54 -0500

    :zap: [ADD] Next/image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Image from 'next/image';
 import Menu from '@components/Menu';
 import MyOrder from '@containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
@@ -13,9 +14,9 @@ const Header = () => {
 	return (
 		<>
 			<nav className={styles.Nav}>
-				<img src={menu} alt="menu" className={styles.menu} />
+				<Image src={menu} alt="menu" className={styles.menu} />
 				<div className={styles['navbar-left']}>
-					<img src={logo} alt="logo" className={styles['nav-logo']} />
+					<Image src={logo} alt="logo" className={styles['nav-logo']} />
 					<ul>
 						<li>
 							<a href="/">All</a>
@@ -46,7 +47,7 @@ const Header = () => {
 							// className={styles.['navbar-shopping-cart']}
 							onClick={() => toggleOrder()}
 						>
-							<img className={styles['more-clickable-area'], styles.pointer} src={shoppingCart} alt="shopping cart" />
+							<Image className={styles['more-clickable-area'], styles.pointer} src={shoppingCart} alt="shopping cart" />
 							{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 						</li>
 					</ul>
