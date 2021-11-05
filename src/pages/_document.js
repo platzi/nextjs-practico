7631263adeb2e85fc49a943328e24320e9476913
Oscar Barajas <oscar@gndx.dev>
2021-11-04 19:51:57 -0500

    :sparkles: [ADD] Next/head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,12 +6,21 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <title>React Shop</title>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZN80WG7H93"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZN80WG7H93');`
+          }} />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
-}
+};
+
+export default MyDocument;
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,24 +1,30 @@
+import Head from 'next/head';
 import React from 'react';
 import OrderItem from '@components/OrderItem';
 import styles from '@styles/Checkout.module.scss';
 
 const Checkout = () => {
   return (
-    <div className={styles.Checkout}>
-      <div className={styles['Checkout-container']}>
-        <h1 className={styles.title}>My order</h1>
-        <div className={styles['Checkout-content']}>
-          <div className={styles.order}>
-            <p>
-              <span>03.25.21</span>
-              <span>6 articles</span>
-            </p>
-            <p>$560.00</p>
+    <>
+      <Head>
+        <title>Checkout</title>
+      </Head>
+      <div className={styles.Checkout}>
+        <div className={styles['Checkout-container']}>
+          <h1 className={styles.title}>My order</h1>
+          <div className={styles['Checkout-content']}>
+            <div className={styles.order}>
+              <p>
+                <span>03.25.21</span>
+                <span>6 articles</span>
+              </p>
+              <p>$560.00</p>
+            </div>
           </div>
+          {/* <OrderItem /> */}
         </div>
-        <OrderItem />
       </div>
-    </div>
+    </>
   );
 }
 
